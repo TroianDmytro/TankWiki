37e40cef053fae00d9bea96d893195d167fc9efc
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-08-07 14:21:34 +0300

    #Added Put and Delete methods in the TurretController

--- Controllers/TurretController.cs
+++ Controllers/TurretController.cs
@@ -30,10 +30,14 @@ namespace TankWiki.Controllers
                 .ThenInclude(tn => tn.Tank)
                 .Select(t => new TurretDTO(t)
                 {
-                    Guns = t.TurretGuns.Select(tg => new GunDTO(tg.Gun)).ToList(),
-                    Tanks = t.TankTurrets.Select(t => new TankDTOTruncated(t.Tank)).ToList()
-                })
-                .ToListAsync();
+                    Guns = t.TurretGuns
+                            .Select(tg => new GunDTO(tg.Gun))
+                            .ToList(),
+                    Tanks = t.TankTurrets
+                             .Select(t => new TankDTOTruncated(t.Tank))
+                             .ToList()
+
+                }).ToListAsync();
 
             //return this.StatusCode((int)HttpStatusCode.OK, turrets);
             return Ok(turrets);
@@ -43,27 +47,32 @@ namespace TankWiki.Controllers
         public async Task<IActionResult> GetById(int turretId)
         {
             var turret = await _dbContext.Turrets
-                .Include(tg => tg.TurretGuns)
-                .ThenInclude(g => g.Gun)
-                .Include(tt => tt.TankTurrets)
-                .ThenInclude(t => t.Tank)
-                .FirstOrDefaultAsync(t => t.TurretId == turretId);
+                                        .Include(tg => tg.TurretGuns)
+                                        .ThenInclude(g => g.Gun)
+                                        .Include(tt => tt.TankTurrets)
+                                        .ThenInclude(t => t.Tank)
+                                        .FirstOrDefaultAsync(t => t.TurretId == turretId);
 
             if (turret == null) return BadRequest("Turret not found.");
           
             var newTurret = new TurretDTO(turret)
             {
-                Guns = turret.TurretGuns.Select(gun => new GunDTO(gun.Gun)).ToList(),
+                Guns = turret.TurretGuns
+                             .Select(gun => new GunDTO(gun.Gun))
+                             .ToList(),
                 
-                Tanks = turret.TankTurrets.Select(t => new TankDTOTruncated(t.Tank)).ToList()
+                Tanks = turret.TankTurrets
+                              .Select(t => new TankDTOTruncated(t.Tank))
+                              .ToList()
             };
 
             return Ok(turret);
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostTurret(string turretName,int tier, int turretFront, int turretSide, int turretRear,
-                                        double turn, int overview, int weight, long price)
+        public async Task<IActionResult> PostTurret(
+                                string turretName,int tier, int turretFront, int turretSide, 
+                                int turretRear, double turn, int overview, int weight, long price)
         {
             var turret = new Turret
             {
@@ -84,7 +93,40 @@ namespace TankWiki.Controllers
             return Ok("Added turret.");
         }
 
-       
-       
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, string? turretName, int? tier, int? turretFront,
+                                             int? turretSide, int? turretRear, double? turn, int? overview,
+                                             int? weight, long? price)
+        {
+            Turret? turret = await _dbContext.Turrets.FindAsync(id);
+
+            if (turret == null) return BadRequest("Turret not found.");
+
+            turret.TurretName = string.IsNullOrEmpty(turretName) || turretName.Equals(turret.TurretName) ?
+                                                                            turret.TurretName : turretName;
+            turret.Tier =(int)( tier??turret.Tier);
+            turret.TurretFront = (int)(turretFront ?? turret.TurretFront);
+            turret.TurretSide = (int)(turretSide ?? turret.TurretSide);
+            turret.TurretRear = (int)(turretRear ?? turret.TurretRear);
+            turret.Turn = (double)(turn ?? turret.Turn);
+            turret.Overview = (int)(overview ?? turret.Overview);
+            turret.Weight = (int)(weight ?? turret.Weight);
+            turret.Price = (long)(price ?? turret.Price);
+
+            _dbContext.Turrets.Update(turret);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Updated turret");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _dbContext.Turrets.Where(t=>t.TurretId == id).ExecuteDeleteAsync();
+            await _dbContext.SaveChangesAsync();
+
+            return Ok("Deleted turret.");
+        }
+
     }
 }
