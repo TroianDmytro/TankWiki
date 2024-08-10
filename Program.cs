c201042dea7002ab2988874d8606bca0e381b2b2
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-08-10 19:52:36 +0300

    #Create Cors default settings

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ using System;
 var builder = WebApplication.CreateBuilder(args);
 //conect to db
 //builder.Services.AddDbContext<MySqlDBContext>(options=>options.UseSqlServer( builder.Configuration.GetConnectionString("SqlConnect")));
-builder.Services.AddDbContext<MySqlDBContext>(options=>options
-                                .UseSqlServer( builder.Configuration.GetConnectionString("AmazonDbConnectionString")));
+builder.Services.AddDbContext<MySqlDBContext>(options => options
+                                .UseSqlServer(builder.Configuration.GetConnectionString("AmazonDbConnectionString")));
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -14,14 +14,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(option =>
+{
+    option.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
@@ -29,4 +40,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors();
 app.Run();
